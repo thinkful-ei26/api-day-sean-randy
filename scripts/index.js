abd58208ad1b3e1164495f4744adeb8e2d1a0797
seankrummel <sean.krummel@gmail.com>
2018-11-07 11:02:56 -0600

    fetched items and added them to the store

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,10 @@
 'use strict';
 
 $(document).ready(function() {
+  api.getItems((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
   shoppingList.bindEventListeners();
   shoppingList.render();
-});
-
-store.items.push(Item.create('apples'));
-
-
-api.createItem('pears', (newItem) => {
-  api.getItems((items) => {
-    console.log(items);
-  });
 });
