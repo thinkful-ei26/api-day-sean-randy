fbbe34e02cc3ea6d356d890c193ac4c93e53f753
seankrummel <sean.krummel@gmail.com>
2018-11-07 10:53:24 -0600

    implemented gitItems and createItem methods

--- scripts/api.js
+++ scripts/api.js
@@ -6,8 +6,19 @@ const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/sean-randy';
 
   function getItems(callback) {
-    callback('api module works');
+    $.getJSON(`${BASE_URL}/items`, callback);
   }
 
-  return {getItems};
+  function createItem(name, callback) {
+    const newItem = JSON.stringify({name});
+    $.ajax({
+      url: `${BASE_URL}/items`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newItem,
+      success: callback
+    });
+  }
+
+  return {getItems, createItem};
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-/* global shoppingList, store, Item */
+/* global shoppingList, store, Item, api */
 /* eslint-env jquery */
 'use strict';
 
@@ -8,3 +8,10 @@ $(document).ready(function() {
 });
 
 store.items.push(Item.create('apples'));
+
+
+api.createItem('pears', (newItem) => {
+  api.getItems((items) => {
+    console.log(items);
+  });
+});
